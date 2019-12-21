751c4a50db7b064ef5847b3a735b0878992d718e
podhmo <ababjam61+github@gmail.com>
2019-12-22 01:47:19 +0900

    chore: only python 3.7+ (#18)

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
         #   5 - Production/Stable
         "Development Status :: 3 - Alpha",
     ],
-    python_requires=">3.5",
+    python_requires=">=3.7",
     packages=find_packages(exclude=["monogusa.tests"]),
     install_requires=install_requires,
     extras_require={
