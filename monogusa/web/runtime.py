72e9e7957891301c4cf1feefeece73449d91ed32
podhmo <ababjam61+github@gmail.com>
2019-12-25 23:14:25 +0900

    Change web response (#30)
    * fix: web, change response's structure
    
    * chore: update examples
    
    * fix: fix for lint

--- monogusa/web/runtime.py
+++ monogusa/web/runtime.py
@@ -1,16 +1,15 @@
+from __future__ import annotations
 import typing as t
 import time
 import contextlib
 import dataclasses
-from io import StringIO
 import pydantic.main as pydantic_main
 import pydantic.class_validators
 
 
 @dataclasses.dataclass
 class State:
-    stdout: StringIO
-    stderr: StringIO
+    console: t.List[_OutputRow]
     start: float  # time.time()
 
     # shell's status code like value.
@@ -21,21 +20,21 @@ class State:
         return {
             "status_code": self.status_code,
             "duration": time.time() - self.start,
-            "stdout": self.stdout.getvalue(),
-            "stderr": self.stderr.getvalue(),
+            "console": self.console,
         }
 
 
 @contextlib.contextmanager
 def handle(*, now: t.Optional[float] = None) -> t.Iterator[State]:
-    stdout = StringIO()
-    stderr = StringIO()
+    box: t.List[_OutputRow] = []
+    stdout = _JSONIO(box, port="stdout")
+    stderr = _JSONIO(box, port="stderr")
     now = now or time.time()
 
-    s = State(start=now, stdout=stdout, stderr=stderr)
+    s = State(start=now, console=box)
     # TODO: use demux like interface, instead of redirect_xxx()
-    with contextlib.redirect_stdout(stdout):
-        with contextlib.redirect_stderr(stderr):
+    with contextlib.redirect_stdout(stdout):  # type: ignore
+        with contextlib.redirect_stderr(stderr):  # type: ignore
             try:
                 yield s
             except Exception:
@@ -47,16 +46,36 @@ def handle(*, now: t.Optional[float] = None) -> t.Iterator[State]:
                 print(tb, file=stderr)
 
 
+class _JSONIO:
+    def __init__(self, box: t.List[_OutputRow], port: str = "stdout") -> None:
+        self.box = box
+        self.port = port
+
+    def write(self, lines: str) -> None:
+        if not lines:
+            return
+        data = _OutputRow.construct(port=self.port, lines=_as_list(lines))
+        self.box.append(data)
+
+    def writelines(self, lines: t.Iterable[str]) -> None:
+        for line in lines:
+            self.write(line)
+
+
 # todo: move it?
+class _OutputRow(pydantic_main.BaseModel):
+    port: str
+    lines: t.List[str]
+
+
 class CommandOutput(pydantic_main.BaseModel):
     status_code: int = 0
-    stdout: t.Union[t.List[str], str]
-    stderr: t.Union[t.List[str], str]
+    console: t.List[_OutputRow]
     duration: float
 
-    @pydantic.class_validators.validator("stdout", "stderr", always=True)
-    def as_clean_list(cls, s: t.Union[t.List[str], str]) -> t.List[str]:
-        return _as_list(s)
+    @pydantic.class_validators.validator("console", always=True)
+    def as_clean_list(cls, rows: t.List[_OutputRow]) -> t.List[_OutputRow]:
+        return [row for row in rows if row.lines]
 
 
 def _as_list(s: t.Union[t.List[str], str]) -> t.List[str]:
