603ebacea25cd58ce8b70d7131849b3a570a2d44
podhmo <ababjam61+github@gmail.com>
2020-02-29 17:59:07 +0900

    chore: edit setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,6 @@
 from setuptools import setup, find_packages
 
 
-install_requires = ["handofcats>=3.1.1", "magicalimport", "prestring"]
-dev_requires = ["black", "flake8", "mypy"]
-tests_requires = ["pytest", "pytest-asyncio"]
-web_requires = ["dictknife", "pydantic", "fastapi", "uvicorn", "async-asgi-testclient"]
-
 setup(
     classifiers=[
         # "License :: OSI Approved :: MIT License",
@@ -18,15 +13,15 @@ setup(
     ],
     python_requires=">=3.7",
     packages=find_packages(exclude=["monogusa.tests"]),
-    install_requires=install_requires,
+    install_requires=["handofcats>=3.2.0", "magicalimport>=0.9.1", "prestring>=0.8.2"],
     extras_require={
-        "testing": tests_requires,
-        "dev": dev_requires,
-        "web": web_requires,
+        "testing": ["pytest", "pytest-asyncio"],
+        "dev": ["black", "flake8", "mypy"],
+        "web": ["dictknife", "pydantic", "fastapi", "uvicorn", "async-asgi-testclient"],
         "slack": ["slackbot", "python-dotenv"],
         "discord": ["discord.py", "python-dotenv"],
     },
-    tests_require=tests_requires,
+    tests_require=["pytest", "pytest-asyncio"],
     test_suite="monogusa.tests",
     #     entry_points="""
     #       [console_scripts]
